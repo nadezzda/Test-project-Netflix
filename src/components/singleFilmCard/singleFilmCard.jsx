d68ef922c2803fc0aa30abd741741ed2cf5d25a4
Nadiia <malicenkonadic@gmail.com>
2023-07-01 18:20:45 +0300

    Create bookmarked page

---
+++ src/components/singleFilmCard/singleFilmCard.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from "next/image";
+import styles from "./singleFilmCard.module.css";
+
+
+const SingleFilmCard = ({name, year, type, rating, img}) => {
+    return (
+        <div className={styles.card}>
+            <Image
+                src={img}
+                alt="movie banner"
+                width={280}
+                height={174}
+                className={styles.banner}
+            />
+            <div className={styles.bookmark}>
+                <Image 
+                    src="/icons/icon-bookmark-full.svg"
+                    alt="bookmark"
+                    width={15}
+                    height={15}
+                />
+            </div>
+            <p>{year} • 
+            {type === "Movie" ? (
+                <Image 
+                    src="/icons/icon-nav-movies.svg"
+                    alt="movies"
+                    width={12}
+                    height={12}
+                />
+            ) : (
+                <Image 
+                    src="/icons/icon-nav-tv-series.svg"
+                    alt="other icon"
+                    width={12}
+                    height={12}
+                />
+            )}
+                {type} • {rating}</p>
+            <h4>{name}</h4>
+        </div>
+    );
+}
+export default SingleFilmCard;
---
+++ src/pages/bookmarked.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import Image from "next/image";
+import SingleFilmCard from "@/components/singleFilmCard/singleFilmCard";
+import styles from "../components/singleFilmCard/singleFilmCard.module.css";
+import CustomInput from "@/components/input/input";
+
+const BookmarkedPage = () => {
+  const movie = {
+    name: 'Relentless',
+    year: 2017,
+    type: 'Movie',
+    img: '/images/Rectangle.png',
+    rating: 'RG'
+  }
+  const tvShow = {
+    name: 'The Rockies',
+    year: 2015,
+    type: 'TV Series',
+    img: '/images/Rectangle2.png',
+    rating: 'E'
+  }
+    return(
+        <section className={styles.section}>
+            <form className={styles.form}>
+                <button className={styles.button}>
+                  <Image
+                    src="/icons/icon-search.svg"
+                    alt="search"
+                    width={32}
+                    height={32}
+                  />
+                </button>
+                <input className={styles.input} placeholder="Search for bookmarked shows"/>
+            </form>
+            <div>
+              <h1>Bookmarked Movies</h1>
+              <div className={styles.content}>
+                <SingleFilmCard name={movie.name} year={movie.year} type={movie.type} rating={movie.rating} img={movie.img}/>
+                <SingleFilmCard name={movie.name} year={movie.year} type={movie.type} rating={movie.rating} img={movie.img}/>
+                <SingleFilmCard name={movie.name} year={movie.year} type={movie.type} rating={movie.rating} img={movie.img}/>
+                <SingleFilmCard name={movie.name} year={movie.year} type={movie.type} rating={movie.rating} img={movie.img}/>
+                <SingleFilmCard name={movie.name} year={movie.year} type={movie.type} rating={movie.rating} img={movie.img}/>
+              </div>
+            </div>
+            <div>
+              <h1>Bookmarked TV-Series</h1>
+              <div className={styles.content}>
+                <SingleFilmCard name={tvShow.name} year={tvShow.year} type={tvShow.type} rating={tvShow.rating} img={tvShow.img}/>
+                <SingleFilmCard name={tvShow.name} year={tvShow.year} type={tvShow.type} rating={tvShow.rating} img={tvShow.img}/>
+                <SingleFilmCard name={tvShow.name} year={tvShow.year} type={tvShow.type} rating={tvShow.rating} img={tvShow.img}/>
+                <SingleFilmCard name={tvShow.name} year={tvShow.year} type={tvShow.type} rating={tvShow.rating} img={tvShow.img}/>
+              </div>
+            </div>
+        </section>
+    );
+}
+
+export default BookmarkedPage;
+
+export async function getStaticProps() {
+    const user = false;
+    if (user) {
+      return {
+        redirect: { destination: "/" },
+        props: {},
+      };
+    }
+    return {
+      props: {},
+    };
+  }
